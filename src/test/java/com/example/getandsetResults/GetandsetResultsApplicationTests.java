a1f6f867991af7a54fe7f4edcb34a418d456f2fa
Sheblyk <kokosigel@gmail.com>
2021-09-02 14:32:51 +0300

    init commit

---
+++ src/test/java/com/example/getandsetResults/GetandsetResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getandsetResults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetandsetResultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
